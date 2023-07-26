395694385b2ea0ce5f178330c63f83ec1e57b33c
d940909492 <dong5455993@gmail.com>
2023-07-26 18:58:24 -0400

    testing

--- Linear Regression/t1.py
+++ Linear Regression/t1.py
@@ -2,3 +2,25 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
+
+# Linear equation: y = mx+b
+
+def calculate_Error(m, b, np_data):
+    totalerror = 0
+    for i in range(0, len(np_data)):
+        x = np_data[i, 0]
+        y = np_data[i, 1]
+        totalerror += (y - (m*x + b)) ** 2
+    return totalerror / len(np_data)
+
+def main():
+    b = 0
+    m = 0
+    n = int(input('Enter the age: '))
+    error_point = calculate_Error()
+
+    y = m * n + error_point
+    print('predict salary is: ' + str(y))
+
+if __name__ == '__main__':
+    main()
