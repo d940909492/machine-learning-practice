306b67f34fed2a17e697616167808568b0d7dad5
d940909492 <dong5455993@gmail.com>
2023-07-30 20:12:17 -0400

    testing mod

--- Linear_Regression/t1.py
+++ Linear_Regression/t1.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
-from Linear_Regression.training_sources import prepare_for_training
+from training_sources.prepare_for_training import prepare_for_training
 import csv
 
 # Linear equation: y = mx+b
 # we need Least squares method to find error
 
+
+"""
 def calculate_Error(m, b, np_data):
     totalerror = 0
     for i in range(0, len(np_data)):
@@ -15,8 +17,9 @@ def calculate_Error(m, b, np_data):
         y = np_data[i, 1]
         totalerror += (y - (m*x + b)) ** 2
     return totalerror / len(np_data)
-
-def calculate_cost(fdata , obj , m):
+"""
+    
+def calculate_error(fdata , obj , m):
     ex_num = fdata.shape[0]
     temp = hyp(fdata, m) - obj
     cost = (1/2)*np.dot(temp.T, temp)/ex_num
@@ -25,10 +28,12 @@ def calculate_cost(fdata , obj , m):
 """
 Need predict var and real(actual) var
 """
-def grad_dec(num_f, m, learning_rate, learning_times , fdata):
+def grad_dec(num_f, m, learning_rate, learning_times , fdata, obj):
+    cost_record = []
     for i in range(learning_times):
         grad_step(m, fdata, learning_rate)
-    return
+        cost_record.append(calculate_error(fdata, obj, m))
+    return cost_record
         
 def grad_step(m, fdata, learning_rate):
     num_ex = fdata.shape[0]
@@ -36,18 +41,23 @@ def grad_step(m, fdata, learning_rate):
     temp_m = m
     temp_m = temp_m - learning_rate * (1/num_ex) * np.dot(pred.T, fdata).T
     m = temp_m
-    return
 
 def hyp(fdata, m):
     pred = np.dot(fdata,m)
     return pred
 
+def training(num_f,m, learning_rate, learning_times, fdata, obj):
+    error_record = grad_dec(num_f,m, learning_rate, learning_times , fdata, obj)
+    return num_f, error_record
 
 ## test and run ##
 def main():
-    fdata = np.genfromtxt('test0.csv', delimiter=',')
+    fdata = np.np.genfromtxt('test0.csv', delimiter=',')
 
-    n = int(input('Enter the age: '))
+    sdata = pd.read_csv('test0.csv')
+
+    ### n = int(input('Enter the age: '))
+
     learning_times = 1000
     learning_rate = 0.001
 
@@ -56,12 +66,30 @@ def main():
 
     obj = fdata[: , num_f - 1]
 
-    grad_dec(num_f,m, learning_rate, learning_times , fdata)
-
+    num_f , error_record = training(num_f,m, learning_rate, learning_times, fdata, obj)   
+        
+    Sdata = prepare_for_training(fdata,0,0,True)[0]
+    
+    Error_var = calculate_error(Sdata,obj)
+         
+    pred = hyp(Sdata,obj)
+    
+    """
     b = calculate_Error()
 
     y = m * n + b
     print('predict salary is: ' + str(y))
+    """
+
+    x_var = sdata[['age']].values
+    y_var = sdata[['salary']].values
+
+    plt.scatter(x_var,y_var,label='data')
+    plt.xlabel('age')
+    plt.ylabel('salary')
+    plt.title('test')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- Linear_Regression/training_sources/prepare_for_training.py
+++ Linear_Regression/training_sources/prepare_for_training.py
@@ -8,12 +8,10 @@ from .generate_polynomials import generate_polynomials
 
 def prepare_for_training(data, polynomial_degree=0, sinusoid_degree=0, normalize_data=True):
 
-    # 计算样本总数
     num_examples = data.shape[0]
 
     data_processed = np.copy(data)
 
-    # 预处理
     features_mean = 0
     features_deviation = 0
     data_normalized = data_processed
@@ -26,17 +24,15 @@ def prepare_for_training(data, polynomial_degree=0, sinusoid_degree=0, normalize
 
         data_processed = data_normalized
 
-    # 特征变换sinusoidal
     if sinusoid_degree > 0:
         sinusoids = generate_sinusoids(data_normalized, sinusoid_degree)
         data_processed = np.concatenate((data_processed, sinusoids), axis=1)
 
-    # 特征变换polynomial
     if polynomial_degree > 0:
         polynomials = generate_polynomials(data_normalized, polynomial_degree, normalize_data)
         data_processed = np.concatenate((data_processed, polynomials), axis=1)
 
-    # 加一列1
+
     data_processed = np.hstack((np.ones((num_examples, 1)), data_processed))
 
     return data_processed, features_mean, features_deviation
